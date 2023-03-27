f3bc53b4304224e9878ba35efd2ec3f8bcc01c20
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-03-27 08:08:45 -0700

    Create learning_1.cpp

---
+++ SET B/LEARNING/learning_1.cpp
@@ -0,0 +1,16 @@
+I got the biggest ever learning while solving the questions
+
+❤️ learning: if array contains negative no then sliding window cant be applied
+
+this question
+862. Shortest Subarray with Sum at Least K
+link :https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
+contains negative no too ...hence sliding window cant be applied in it
+
+this contains only positive no
+209. Minimum Size Subarray Sum
+link :https://leetcode.com/problems/minimum-size-subarray-sum/description/
+hence sliding window can be applied
+
+
+the above two questions are exactly same.....the only difference is one contains +ve no only and the other -ve too
